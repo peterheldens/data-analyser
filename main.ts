5e4ca61ed4dadc54bbcb6044841c9732aafb86fb
Peter Heldens <peter.heldens@live.nl>
2020-07-05 12:57:48 +0200

    add changes a.x a.y

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
 basic.forever(function () {
     serial.writeValue("x", input.acceleration(Dimension.X))
     serial.writeValue("y", input.acceleration(Dimension.Y))
+    serial.writeValue("a.x", input.acceleration(Dimension.X))
+    serial.writeValue("a.y", input.acceleration(Dimension.Y))
 })
